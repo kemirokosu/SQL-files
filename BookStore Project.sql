bf337a0779ece73255b8486b9d3a9cb55f94c8ed
Oluwakemi Rokosu <105376138+kemirokosu@users.noreply.github.com>
2023-02-20 19:26:49 +0100

    Update BookStore Project.sql

--- BookStore Project.sql
+++ BookStore Project.sql
@@ -5,7 +5,7 @@ BookName varchar(50), Publisher varchar(50),
 ISBN varchar(50),Edition varchar(50), NumberofPages int,
 City varchar(50), PriceinDollar float)
 
---Inserting Info into columns
+--Inserting values into table
 INSERT INTO BookStoreAnalysis1 VALUES
 (1,'Legendborn','Margaret K. McElderry Books',1534441611,'1st',544,'New York',22.09),
 (2,'Psychology of Money','Harriman House',0857197681,'1st',256,'Wilmington', 12.66),
@@ -21,12 +21,45 @@ INSERT INTO BookStoreAnalysis1 VALUES
 (12,'The Alchemist','HarperOne',0062315005,'1st',208,'San Francisco',12.28),
 (13,'How to talk to anyone','McGraw Hill',9780007272617,'1st',367,'New York',9.99)
 
---Querying the most expensive book and book with the most pages
-SELECT MAX(PriceinDollar)AS maximumprice,MAX(NumberofPages) AS Highestnumberofpages,
+--Finding the most expensive book 
+SELECT MAX(PriceinDollar)AS maximumprice,BookName
 FROM SQLTutorial.dbo.BookStoreAnalysis1
+GROUP BY BookName
+ORDER BY MAX(PriceinDollar) DESC
 
- --The range of prices of the books: From most expensive to least expensive
- SELECT BookName,NumberofPages,PriceinDollar
- FROM SQLTutorial.dbo.BookStoreAnalysis1
- ORDER BY PriceinDollar DESC
+--Querying to know the least expensive book
+SELECT MIN(PriceinDollar)AS maximumprice,BookName
+FROM SQLTutorial.dbo.BookStoreAnalysis1
+GROUP BY BookName
+ORDER BY MIN(PriceinDollar) ASC
+
+--Adding the rankings and genres to determine most preferred and least preferred and the highest ranking genres
+ALTER TABLE BookStoreAnalysis1 
+ADD Genre varchar(50), Rankings varchar(50)
+
+UPDATE BookStoreAnalysis1 SET Genre = 'Fantasy',Rankings = '6032' WHERE BookId = 1
+UPDATE BookStoreAnalysis1 SET Genre = ' Finances',Rankings = '80' WHERE BookId = 2
+UPDATE BookStoreAnalysis1 SET Genre = 'Self Help', Rankings = '106' WHERE BookId = 3
+UPDATE BookStoreAnalysis1 SET Genre = 'Spiritual', Rankings = '563337' WHERE BookId = 4
+UPDATE BookStoreAnalysis1 SET Genre = 'Fantasy', Rankings = '592033' WHERE BookId = 5
+UPDATE BookStoreAnalysis1 SET Genre = 'Spiritual', Rankings = '37022' WHERE BookId = 6
+UPDATE BookStoreAnalysis1 SET Genre = 'Self Help', Rankings = '968' WHERE BookId = 7
+UPDATE BookStoreAnalysis1 SET Genre = 'Spiritual and Dating', Rankings = '2042843' WHERE BookId = 8
+UPDATE BookStoreAnalysis1 SET Genre = 'Self Help', Rankings = '5948' WHERE BookId = 9
+UPDATE BookStoreAnalysis1 SET Genre = 'Romance', Rankings = '27206' WHERE BookId = 10
+UPDATE BookStoreAnalysis1 SET Genre = 'Self Help', Rankings = '283' WHERE BookId = 11
+UPDATE BookStoreAnalysis1 SET Genre = 'Fantasy', Rankings = '115' WHERE BookId = 12
+UPDATE BookStoreAnalysis1 SET Genre = 'Self Help', Rankings = '616' WHERE BookId = 13
+
+ALTER TABLE BookStoreAnalysis1
+ALTER COLUMN Rankings int
+
+--Querying to find out which books are Top ranked which translates to most preferred
+SELECT BookName, Genre,Rankings
+FROM BookStoreAnalysis1
+ORDER BY Rankings ASC
+
+
+SELECT * 
+FROM BookStoreAnalysis1
 
